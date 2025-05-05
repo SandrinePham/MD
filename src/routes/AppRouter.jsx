41c15e6be5ada490f80057ea4876722f20271e66
Sandrine P <sandrine.pham.89@gmail.com>
2025-05-05 19:07:01 +0200

    GitHub Page 3

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,23 +1,22 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import About from "../pages/About";
 import Blog from "../pages/Blog";
 import Contact from "../pages/Contact";
 import Prestations from "../pages/Prestations";
-
 import Layout from "../layouts/Layout";
 
 function AppRouter() {
   return (
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/prestations" element={<Prestations />} />
-        </Route>{" "}
-      </Routes>
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/prestations" element={<Prestations />} />
+      </Route>
+    </Routes>
   );
 }
 
