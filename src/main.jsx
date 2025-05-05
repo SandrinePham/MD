929fdd19b25e685cf3994fb05bc4692ab1517342
Sandrine P <sandrine.pham.89@gmail.com>
2025-05-05 18:59:55 +0200

    GitHub Page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.scss'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // 🆕 import
+import './index.scss';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter basename="/MD">  {/* 🆕 basename = nom du repo */}
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
